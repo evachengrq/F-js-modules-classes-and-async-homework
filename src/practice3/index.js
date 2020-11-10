d60eac1889f6f75b21c8fb3d2ec1059a4b574d3e
Ruoqian Cheng <ruoqian.cheng@RuoqiandeMacBook-Pro.local>
2020-11-10 23:47:12 +0800

    [evacheng]feat: finished task 3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,15 @@
-export const getPoetry
+export const getPoetry = () => {
+  return fetch("https://v1.jinrishici.com/all.json")
+  .then(response => {
+    if(response.status == 200) {
+      return response.json()
+    }
+    return Promise.reject()
+  }).then(data => {
+    const {origin, author, content} = data;
+    return [origin, author, content];
+  })
+  .catch(error => {
+    console.log(error)
+  })
+}
