90a932e34f4e62531e96db4d88bd696938f925d2
Ruoqian Cheng <ruoqian.cheng@RuoqiandeMacBook-Pro.local>
2020-11-19 14:17:36 +0800

    [evacheng]feat: rewrite with then n catch and asunc n await

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,15 +1,29 @@
-export const getPoetry = () => {
-  return fetch("https://v1.jinrishici.com/all.json")
-  .then(response => {
-    if(response.status == 200) {
-      return response.json()
-    }
-    return Promise.reject()
-  }).then(data => {
-    const {origin, author, content} = data;
+// export const getPoetry = () => {
+//   const result = fetch("https://v1.jinrishici.com/all.json")
+//   .then(response => {
+//     if (response.status === 200) {
+//       return response.json()
+//     } else {
+//       console.log(response.status)
+//     }
+//   })
+//   .then(json => {
+//     const {content, origin, author} = json;
+//     return [origin, author, content]
+//   })
+//   .catch(err => console.log(err));
+//   return result;
+// }
+
+export async function getPoetry() {
+
+  try {
+    const response = await fetch("https://v1.jinrishici.com/all.json");
+    const json = await response.json();
+    const {content, origin, author} = json;
     return [origin, author, content];
-  })
-  .catch(error => {
-    console.log(error)
-  })
+  } catch(err) {
+    console.log(err)
+  }
+  
 }
